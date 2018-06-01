b027c2c263ef820fd189d54448246f1f0c2e1344
Brandon Stewart <boveus@gmail.com>
2018-06-01 18:52:02 -0400

    add liquor bottle component, remove extra stuff from app.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,19 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
 import WineGlass from './components/wine_glass'
 import BeerGlass from './components/beer_glass'
+import LiquorBottle from './components/liquor_bottle'
 import './App.css'
 
 class App extends Component {
   render () {
     return (
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h1 className='App-title'>Welcome to React</h1>
-        </header>
-        <p className='App-intro'>
-          <div class='wrap'>
-            <h3> Wine Glass </h3>
-            <WineGlass />
-            <h3> Beer Glass </h3>
-            <BeerGlass />
-            <h3> Liquor Bottle </h3>
-            <div class='neck' />
-            <div class='bottle' />
-            <div class='spirit' />
-          </div>
-        </p>
+      <div class='wrap'>
+        <h3> Wine Glass </h3>
+        <WineGlass />
+        <h3> Beer Glass </h3>
+        <BeerGlass />
+        <h3> Liquor Bottle </h3>
+        <LiquorBottle />
       </div>
     )
   }
---
+++ src/components/liquor_bottle.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class LiquorBottle extends Component {
+  render () {
+    return (
+      <div className='liquor-bottle'>
+        <div className='neck' />
+        <div className='bottle' />
+        <div className='spirit' />
+      </div>
+    )
+  }
+}
+
+export default LiquorBottle
