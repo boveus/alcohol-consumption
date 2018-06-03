fcf9746062705fe6b3bf4ab1ea197f276fa0e61b
Brandon Stewart <boveus@gmail.com>
2018-06-03 16:32:48 -0400

    modify beer glass behavior, it now fills dynamically

--- src/components/beer_glass.js
+++ src/components/beer_glass.js
@@ -13,24 +13,18 @@ class BeerGlass extends Component {
     let height = 170 * this.state.fill
     return [height, topPadding]
   }
-  // componentDidUpdate (previousProps, previousState) {
-  //   let pcg = this.state.fill / 376
-  //   if (previousProps.data !== this.props.data) {
-  //     console.log('hit it')
-  //     this.setState({height: `${170 * pcg}px`})
-  //     this.setState({paddingTop: `${(170 * pcg) + 30}px`})
-  //   }
-  // }
 
   render () {
     return (
       <div className='beer-glass'>
-        <div className='beerglass' style={{height: `${(170 * (this.state.fill / 376))}px`}} />
+        <div className='beerglass' style={{height: `${170 - (170 * (this.state.fill / 376))}px`}} />
         <div className='handle' />
-        <span className='beer' style={{topPadding: `${(170 * (this.state.fill / 376) + 30)}px`}} />
+        <span className='beer' style={{height: `${(170 * (this.state.fill / 376) + 30)}px`}} />
       </div>
     )
   }
 }
+
+
 
 export default BeerGlass
